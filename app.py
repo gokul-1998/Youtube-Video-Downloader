35101bdb3817db4d654d39631e015e2c7263bc10
gokul-1998 <gokulakrishnanm1998@gmail.com>
2023-08-10 01:43:12 +0530

    done

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from pytube import YouTube
-
+import os
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -9,6 +9,11 @@ def index():
         link = request.form.get('link')
         if link:
             try:
+                videos_folder = 'static/videos'
+                for file_name in os.listdir(videos_folder):
+                    if file_name.endswith(".mp4"):
+                        file_path = os.path.join(videos_folder, file_name)
+                        os.remove(file_path)
                 yt = YouTube(link)
                 stream = yt.streams.get_highest_resolution()
                 video_title = yt.title+".mp4"
